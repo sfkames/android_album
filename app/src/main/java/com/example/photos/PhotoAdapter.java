8d4dda7b1df24be3fc7770adb8cb1f33b4065ab3
Samantha Ames <samanthaames@nbp-130-201.nbp.ruw.rutgers.edu>
2021-12-05 17:14:15 -0500

    Photos View, etc.

---
+++ app/src/main/java/com/example/photos/PhotoAdapter.java
@@ -0,0 +1,2 @@
+package com.example.photos;public class PhotoAdapter {
+}
---
+++ app/src/main/java/com/example/photos/SlideShow.java
@@ -0,0 +1,13 @@
+package com.example.photos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SlideShow extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
